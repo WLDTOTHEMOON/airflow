ce6eed2af808f0b3d5c6041f4d11ff29eb15d24e
Fang Yongchao_fx <yongchao.fang@outlook.com>
2025-04-02 18:19:09 +0800

    update

--- dags/dwd_etl/dwd_ks_cps_order.py
+++ dags/dwd_etl/dwd_ks_cps_order.py
@@ -10,7 +10,8 @@ ods_ks_cps_order_dataset = Dataset('ods_ks_cps_order_dataset')
 ods_ks_leader_order_dataset = Dataset('ods_ks_leader_order_dataset')
 
 @dag(schedule=[ods_ks_cps_order_dataset, ods_ks_leader_order_dataset], start_date=pendulum.datetime(2023, 1, 1), catchup=False,
-     default_args={'owner': 'Fang Yongchao'}, tags=['dwd', 'etl'])
+     default_args={'owner': 'Fang Yongchao'}, tags=['dwd', 'etl'],
+     max_active_runs=1)
 def dwd_ks_cps_order():
     from airflow.models import Variable
     from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
