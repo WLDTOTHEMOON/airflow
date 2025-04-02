9d143640cd47dfca8e565f45318531d271ec4e01
Fang Yongchao_fx <yongchao.fang@outlook.com>
2025-04-02 15:36:18 +0800

    update

---
+++ dags/dim_etl/sql/dim_ks_account_info.sql
@@ -0,0 +1,78 @@
+insert into dim.dim_ks_account_info (
+    account_id, account_status, anchor_name, anchor_status, line, anchor_type,
+    group_leader, specific_bd, specific_op, anchor_commission_rate, organization_commission_rate,
+    other_commission_rate, other_commission_belong, open_id, update_at
+)
+select 
+    account_id
+    ,case src.status
+        when 1 then '已授权'
+        when 0 then '未授权'
+    end account_status
+    ,an.name anchor_name
+    ,case info.status
+        when 1 then '合作中'
+        when 2 then '解约中'
+        when 3 then '已解约'
+    end anchor_status
+    ,case info.line
+        when 0 then '特殊主播'
+        when 1 then '直播电商'
+        when 2 then '切片'
+        when 3 then '其它'
+    end line
+    ,anchor_type
+    ,gl.name group_leader
+    ,sb.name specific_bd
+    ,so.name specific_op
+    ,anchor_commission_rate
+    ,organization_commission_rate
+    ,other_commission_rate
+    ,ocb.name other_commission_belong
+    ,open_id
+    ,if(src.update_at >= info.updated_at, src.update_at, info.updated_at) update_at
+from (
+    select 
+        account_id
+        ,anchor_id
+        ,open_id
+        ,status
+        ,update_at
+    from ods.ods_ks_account_info
+) src
+left join (
+    select 
+        anchor_id
+        ,anchor_type
+        ,group_leader
+        ,specific_bd
+        ,specific_op
+        ,anchor_commission_rate
+        ,organization_commission_rate
+        ,other_commission_rate
+        ,other_commission_belong
+        ,status
+        ,line
+        ,updated_at
+    from ods.ods_ks_anchor_info
+) info on src.anchor_id = info.anchor_id
+left join (
+    select id, name
+    from ods.ods_platform_users opu
+) an on info.anchor_id = an.id
+left join (
+    select id, name
+    from ods.ods_platform_users opu
+) gl on info.group_leader = gl.id
+left join (
+    select id, name
+    from ods.ods_platform_users opu
+) sb on info.specific_bd = sb.id
+left join (
+    select id, name
+    from ods.ods_platform_users opu
+) so on info.specific_op = so.id
+left join (
+    select id, name
+    from ods.ods_platform_users opu
+) ocb on info.other_commission_belong = ocb.id;
--- dags/ods_sync/ods_platform.py
+++ dags/ods_sync/ods_platform.py
@@ -160,13 +160,25 @@ def ods_platform():
         sql = generate_upsert_template('ods', 'ods_pf_account_info')
         read_and_sync(path=path, sql=sql)
 
+    @task(retries=5, retry_delay=10)
+    def ods_pf_users(**kwargs):
+        begin_time = kwargs['data_interval_start']
+        end_time = kwargs['data_interval_end']
+        begin_time_fmt = begin_time.in_tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')
+        end_time_fmt = end_time.in_tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')
+        file_name = fetch_from_source(table='xlsd.users', start_time=begin_time_fmt, end_time=end_time_fmt)
+        path = write_to_cos(file_name=file_name, path='platform/users/')
+        sql = generate_upsert_template('ods', 'ods_pf_users')
+        read_and_sync(path=path, sql=sql)
+
+
     @task(outlets=[ods_platform_dataset])
     def task_finished():
         logger.info(f'platform 相关数据ods更新完成')
 
 
-    ods_pf_links() >> ods_pf_suppliers()
-    ods_pf_products() >> ods_pf_reviews() >> ods_pf_anchor_select_products()
+    ods_pf_links() >> ods_pf_suppliers() >> ods_pf_users() >> \
+    ods_pf_products() >> ods_pf_reviews() >> ods_pf_anchor_select_products() >> \
     ods_pf_anchor_info() >> ods_pf_account_info() >> task_finished()
 
 
